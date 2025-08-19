268a9512b00301fcce5ec01bd02a904039029942
LuquinhasAMC <168360461+LuquinhasAMC@users.noreply.github.com>
2025-08-19 20:43:34 -0300

    Melhoria

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const navbar = document.getElementById("navPrincipal");
 const navBarBottom = document.getElementById("navPrincipalBottom");
 const mainContainerLayout = document.getElementById("mainContainerLayout");
+const fabAddBottom = document.getElementById("fabAdd-bottom");
 
 function ajustarNavbar() {
   if (window.innerWidth <= 780) {
@@ -8,6 +9,7 @@ function ajustarNavbar() {
     navBarBottom.style.display = "flex";
     mainContainerLayout.style.paddingLeft = "0px";
     mainContainerLayout.style.paddingBottom = navBarBottom.offsetHeight + "px";
+    fabAddBottom.style.bottom = navBarBottom.offsetHeight + 16 + "px"; // Adjust FAB position
     document.body.style.paddingBottom = navBarBottom.offsetHeight + "px";
   } else {
     navbar.style.display = "flex";
