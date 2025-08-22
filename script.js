17bcf285467aa9c96b423905ca7668d8d8e3c8ea
LuquinhasAMC <168360461+LuquinhasAMC@users.noreply.github.com>
2025-08-21 22:39:21 -0300

    Pequenas adições

--- script.js
+++ script.js
@@ -3,10 +3,31 @@ const navBarBottom = document.getElementById("navPrincipalBottom");
 const mainContainerLayout = document.getElementById("mainContainerLayout");
 const fabAddBottom = document.getElementById("fabAdd-bottom");
 const divDialogContent = document.querySelectorAll(".dialogContainerContente");
+const contentVewerLists = document.getElementById("contentVewerLists");
 
 let configArray = [{
   "darkMode": false
-}]
+}];
+
+let arrayListItems = [{
+  "type": "LS",
+  "completed": false,
+  "title": "Exemplo",
+  "description": "Esta é uma lista de exemplo.",
+  "itemsForLC": []
+}, {
+  "type": "LS",
+  "completed": true,
+  "title": "Exemplo",
+  "description": "Esta é uma lista de exemplo.",
+  "itemsForLC": []
+}
+]
+
+const filterListLS = arrayListItems.filter(item => item.type === "LS");
+const filterListLC = arrayListItems.filter(item => item.type === "LC");
+const filterListCompleted = arrayListItems.filter(item => item.completed === true);
+const filterListInProgress = arrayListItems.filter(item => item.completed === false);
 
 if (localStorage.getItem("configArray")) {
   configArray = JSON.parse(localStorage.getItem("configArray"));
